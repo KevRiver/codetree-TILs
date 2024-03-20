b719496ff51c53ac2bb61a5d0cc1b18267d1583a
KevRiver <gon1214@sju.ac.kr>
2024-03-20 21:17:50 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240320/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -17,7 +17,7 @@ int main() {
     // dp[i][j] = (i, j)에 도달했을 때 지금까지의 거쳐온 숫자의 최소값
     // dp[i-1][j]에 도달 했을 때의 최소값, dp[i][j-1]의 최소값 중 큰 것과 grid[i][j]를 비교
     // min(max(dp[i-1][j], dp[i][j-1]), grid[i][j]);
-    memset(dp, MAX_VAL, sizeof(dp));
+    memset(dp, MAX_VAL+1, sizeof(dp));
     for(int i=1; i<=n; ++i){
         for(int j=1; j<=n; ++j){
             dp[i][j] = min(max(dp[i-1][j], dp[i][j-1]), grid[i][j]);
