f2ad89e357787319e5b3c4abef99631cf1d20521
KevRiver <gon1214@sju.ac.kr>
2024-03-20 23:37:17 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 240320/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -1,54 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
 int grid[100][100];
-struct Node{
-    int min = 0;
-    int max = 0;
-    int abs(){
-        return max-min;
-    }
-};
-Node dp1[100][100]; // 최대값이 최소일 때
-Node dp2[100][100]; // 최소값이 최대일 때
+int dp[100][100];
 
-Node merge(const Node& a, const Node& b){
-    Node result = {min(a.min, b.min), max(a.max, b.max)};
-    return result;
+
+int simulate(int lower_bound){
+    
+    function<int(int)> flatmap = [=](int num){
+        return num >= lower_bound ? num : INT_MAX;
+    };
+
+    dp[0][0] = flatmap(grid[0][0]);
+    for(int j=1; j<N; ++j){
+        int cur = flatmap(grid[0][0]);
+        dp[0][j] = max(dp[0][j-1], cur);
+    }
+    for(int i=1; i<N; ++i){
+        int cur = flatmap(grid[i][0]);
+        dp[i][0] = max(dp[i-1][0], cur);
+    }
+
+    for(int i=1; i<N; ++i){
+        for(int j=1; j<N; ++j){
+            int cur = flatmap(grid[i][j]);
+            dp[i][j] = max(min(dp[i-1][j], dp[i][j-1]), cur);
+        }
+    }
+    return dp[N-1][N-1];
 }
 
 int main() {
-    int n; cin >> n;
-
-    for(int i=0; i<n; ++i)
-        for(int j=0; j<n; ++j)
+    
+    cin >> N;
+    for(int i=0; i<N; ++i)
+        for(int j=0; j<N; ++j)
             cin >> grid[i][j];
-
-
-    for(int i=0; i<n; ++i){
-        for(int j=0; j<n; ++j){
-            int cur = grid[i][j];
-            if(i > 0 && j > 0){
-                const Node& less_max = dp1[i-1][j].max <= dp1[i][j-1].max ? dp1[i-1][j] : dp1[i][j-1];
-                dp1[i][j] = merge(less_max, {cur, cur});
-                const Node& greater_min = dp2[i-1][j].min >= dp2[i][j-1].min ? dp2[i-1][j] : dp2[i][j-1];
-                dp2[i][j] = merge(greater_min, {cur, cur});
-            } else if(i > 0){
-                dp1[i][j] = merge(dp1[i-1][j], {cur, cur});
-                dp2[i][j] = merge(dp2[i-1][j], {cur, cur});
-            } else if(j > 0){
-                dp1[i][j] = merge(dp1[i][j-1], {cur, cur});
-                dp2[i][j] = merge(dp2[i][j-1], {cur, cur});
-            } else {
-                dp1[i][j] = {cur, cur};
-                dp2[i][j] = {cur, cur};
-            }
-        }
-    }
-    int v = dp1[n-1][n-1].abs();
-    int w = dp2[n-1][n-1].abs();
-    int answer = min(v, w);
+    
+    int answer = INT_MAX;
+    for(int lower_bound = 1; lower_bound <= 100; ++lower_bound){
+        int upper_bound = simulate(lower_bound);
+        if(upper_bound == INT_MAX) continue;
+        answer = min(answer, upper_bound - lower_bound);
+    }    
     cout << answer << '\n';
-
     return 0;
 }
