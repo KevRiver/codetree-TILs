c9b6047488ad5e84fbedf9a7ccacadfa952c187e
KevRiver <gon1214@sju.ac.kr>
2024-03-20 21:07:00 +0900

    Title: 정수 사각형 최소 합, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
+++ 240320/정수 사각형 최소 합/minimum-sum-path-in-square.cpp
@@ -9,10 +9,14 @@ int main() {
     for(int i=1; i<=n; ++i)
         for(int j=1; j<=n; ++j)
             cin >> grid[i][j];
+
     for(int i=1; i<=n; ++i){
         for(int j=n; j>=1; --j){
-            int less = dp[i][j+1]; // left
-            if(i > 1) less = min(less, dp[i-1][j]);
+            int less = 0;
+            if(i > 1 && j < n) less = min(dp[i-1][j], dp[i][j+1]);
+            else if(i > 1) less = dp[i-1][j];
+            else if(j < n) less = dp[i][j+1];
+
             dp[i][j] = less + grid[i][j];
         }
     }
