2385158e853f9fd7db1d100561524c33e6249b37
KevRiver <gon1214@sju.ac.kr>
2024-03-24 15:32:09 +0900

    Title: 작지만 큰 숫자, Time: 299ms, Memory: 5MB, Status: Passed - CodeTree

--- 240324/작지만 큰 숫자/small-but-big-number.cpp
+++ 240324/작지만 큰 숫자/small-but-big-number.cpp
@@ -2,35 +2,27 @@
 using namespace std;
 
 int main() {
-    int n, m; cin >> n >> m;
+    int n, m; 
+    cin >> n >> m;
 
-    vector<int> seq(n);
-    for(int i=0; i<n; ++i) cin >> seq[i];
-    
-    sort(begin(seq), end(seq));
-
-    for(int reps = 0; reps < m; ++reps){
-        int q; cin >> q;
-        
-        if(seq.empty()){
-            cout << -1 << '\n';
-            continue;
-        }
-
-        auto it = lower_bound(begin(seq), end(seq), q);
-
-        // 모든 원소가 q 보다 크다
-        if(it == seq.begin() && *it > q){
-            cout << -1 << '\n';
-            continue;
-        }
-
-        // 모든 원소가 q 보다 작거나 현재 it 값이 q보다 크다
-        if(it == seq.end() || *it > q){
-            advance(it, -1);
-        } 
-        cout << *it << '\n';
-        seq.erase(it);        
+    set<int> s;
+    for(int i=0; i<n; ++i){
+        int x; cin >> x;
+        s.insert(x);
     }
+    
+    for(int i=0; i<m; ++i){
+        int x;
+        cin >> x;
+        auto it = s.upper_bound(x);
+        if(it == s.begin()){
+            cout << -1 << '\n';
+            continue;
+        }
+        --it;
+        cout << *it << '\n';
+        s.erase(it);
+    }
+    
     return 0;
 }
