1bd7e5f3fea20669ecd9669d63a57e17d3d6f79e
KevRiver <gon1214@sju.ac.kr>
2024-03-24 14:52:31 +0900

    Title: 낮은 지점들, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240324/낮은 지점들/lowest-points.cpp
+++ 240324/낮은 지점들/lowest-points.cpp
@@ -1,33 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pii pair<int, int>
-#define ll long long
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    auto cmp = [](const pii& l, const pii& r){
-        return l.second > r.second;
-    };
-    priority_queue<pii, vector<pii>, decltype(cmp)> pq(cmp);
-
-    int n; cin >> n;
+    unordered_map<int, int> min_y;
     for(int i=0; i<n; ++i){
         int x, y;
         cin >> x >> y;
-        pq.push({x, y});
+        if(min_y.find(x) == min_y.end()){
+            min_y[x] = y;
+        } else {
+            min_y[x] = min(min_y[x], y);
+        }
     }
-    unordered_set<int> used;
-    ll sum = 0;
-    while(!pq.empty()){
-        pii e = pq.top();
-        pq.pop();
-        if(used.find(e.first) != used.end()) continue;
-        used.insert(e.first);
-        sum += e.second;
+    long long sum = 0;
+    for(auto it = min_y.begin(); it != min_y.end(); ++it){
+        sum += it->second;
     }
-    cout << sum << '\n';
+    cout << sum;
+    
     return 0;
 }
