3f388d4099b6076e4cc058c0f387747350c92bfd
KevRiver <gon1214@sju.ac.kr>
2024-03-27 19:41:01 +0900

    Title: 점 개수 세기, Time: 1043ms, Memory: 3MB, Status: Time Limit Exceed - CodeTree

--- 240327/점 개수 세기/count-number-of-points.cpp
+++ 240327/점 개수 세기/count-number-of-points.cpp
@@ -5,19 +5,27 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int n, q;
-    cin >> n; cin >> q;
+    int n; cin >> n;
+    int q; cin >> q;
 
     set<int> s;
     for(int i=0; i<n; ++i){
         int pos; cin >> pos;
         s.insert(pos);
     }
+    // 정렬 후 two pointer? -> nlogn + n^2 -> 시간초과
+
+    // lower, upper bound 활용 -> 입력: O(nlogn), lower/upper bound: O(logn)
+    // -> nlogn + q(2logn), where n, q are maximum 100'000
+    // -> O(3nlogn) -> 3 * 100'000 * log(100'000 ~= 1024*128 = 2^17)
+    // 3nlogn ~= 5'100'000 왜 시간초과???
     for(int i=0; i<q; ++i){
-        int a, b;
-        cin >> a >> b;
+        int a; cin >> a;
+        int b; cin >> b;
+
         auto it = s.lower_bound(a);
         auto jt = s.upper_bound(b);
+
         int cnt = distance(it, jt);
         cout << cnt << '\n';
     }
