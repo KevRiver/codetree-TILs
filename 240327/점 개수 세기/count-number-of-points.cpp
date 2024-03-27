18b10454ee6e92385e1497a47d40ad725199ffce
KevRiver <gon1214@sju.ac.kr>
2024-03-27 20:03:19 +0900

    Title: 점 개수 세기, Time: 87ms, Memory: 1MB, Status: Passed - CodeTree

--- 240327/점 개수 세기/count-number-of-points.cpp
+++ 240327/점 개수 세기/count-number-of-points.cpp
@@ -1,32 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int pos[100'000];
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
     int n; cin >> n;
     int q; cin >> q;
-
-    set<int> s;
     for(int i=0; i<n; ++i){
-        int pos; cin >> pos;
-        s.insert(pos);
+        cin >> pos[i];
     }
+    sort(pos, pos+n);
     // 정렬 후 two pointer? -> nlogn + n^2 -> 시간초과
 
     // lower, upper bound 활용 -> 입력: O(nlogn), lower/upper bound: O(logn)
     // -> nlogn + q(2logn), where n, q are maximum 100'000
     // -> O(3nlogn) -> 3 * 100'000 * log(100'000 ~= 1024*128 = 2^17)
     // 3nlogn ~= 5'100'000 왜 시간초과???
+    function<int(int)> lb = [&](int k){
+        int first = 0, last = n;
+        int idx = last;
+        while(first < last){
+            int mid = (first + last)/2;
+            if(pos[mid] < k){
+                first = mid+1;
+            } else {
+                idx = mid;
+                last = mid;
+            }   
+        }
+        return idx;
+    };
+    function<int(int)> ub = [&](int k){
+        int first = 0, last = n;
+        int idx = last;
+        while(first < last){
+            int mid = (first + last)/2;
+            if(pos[mid] <= k){
+                first = mid+1;
+            } else {
+                idx = mid;
+                last = mid;
+            }
+        }
+        return idx;
+    };
     for(int i=0; i<q; ++i){
         int a; cin >> a;
         int b; cin >> b;
-
-        auto it = s.lower_bound(a);
-        auto jt = s.upper_bound(b);
-
-        int cnt = distance(it, jt);
+        
+        int cnt = ub(b) - lb(a);
         cout << cnt << '\n';
     }
     return 0;
