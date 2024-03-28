561e52a20e7685b2503f77508885381f983854b8
KevRiver <gon1214@sju.ac.kr>
2024-03-28 23:02:52 +0900

    Title: 서로 다른 구간의 수, Time: 156ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240328/서로 다른 구간의 수/number-of-distinct-segments.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+    map<int, int> m;
+    for(int i=0; i<N; ++i){
+        int a; cin >> a;
+        int b; cin >> b;
+        m[a]++;
+        m[b]--;
+    }
+
+    int acc = 0;
+    int cnt = 0;
+    for(auto it = m.begin(); it != m.end(); ++it){
+        acc += it->second;
+        if(acc == 0) ++cnt;
+    }
+    cout << cnt << '\n';
+    
+
+    return 0;
+}
