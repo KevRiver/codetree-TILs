65a803b718996b649dc93e4f7fe1fbb857ee88da
KevRiver <gon1214@sju.ac.kr>
2024-03-28 22:35:00 +0900

    Title: 가장 많이 겹치는 구간, Time: 71ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240328/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+
+int x[100'001];
+
+int main() {
+
+    int n; cin >> n;
+    int min_x = 100'001;
+    int max_x = -1;
+    for(int i=0; i<n; ++i){
+        int a; cin >> a;
+        int b; cin >> b;
+        
+        min_x = min(min_x, a);
+        max_x = max(max_x, b);
+
+        x[a]++;
+        x[b+1]--;
+    }
+    int answer = 0;
+    int acc = 0;
+    for(int i=min_x; i<=max_x; ++i){
+        acc += x[i];
+        answer = max(answer, acc);
+    }
+    cout << answer << '\n';
+    
+    return 0;
+}
