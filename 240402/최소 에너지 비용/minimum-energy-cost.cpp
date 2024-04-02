4e1afd86ce3f05dc53ec95fc7781f9a9cd4b49e6
KevRiver <gon1214@sju.ac.kr>
2024-04-02 21:09:10 +0900

    Title: 최소 에너지 비용, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240402/최소 에너지 비용/minimum-energy-cost.cpp
+++ 240402/최소 에너지 비용/minimum-energy-cost.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main() {
     int n; cin >> n;
-    vector<int> energy(n, 0);
+    vector<ll> energy(n, 0);
     for(int i=0; i<n-1; ++i) cin >> energy[i];
-    vector<int> cost(n, 0); // 에너지 1을 채우는 비용
+    vector<ll> cost(n, 0); // 에너지 1을 채우는 비용
     for(int i=0; i<n; ++i) cin >> cost[i];
 
-    vector<int> e(n, 0);
+    vector<ll> e(n, 0);
     e[n-1] = 0;
     for(int i=n-2; i>=0; --i){
         e[i] = e[i+1] + energy[i];
     }
 
-    vector<int> c(n, 0);
+    vector<ll> c(n, 0);
     c[n-1] = 0;
     for(int i=n-2; i>=0; --i){
         c[i] = min(cost[i]*e[i], cost[i]*energy[i] + c[i+1]);
