10f29489a867a264f8a00f7bbf419394bcf1e41c
KevRiver <gon1214@sju.ac.kr>
2024-04-10 21:53:56 +0900

    Title: 바구니 안의 사탕, Time: 87ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240410/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+
+    vector<pair<int, int>> candies;
+    for(int i=0; i<n; ++i){
+        int cnt, x;
+        cin >> cnt >> x;
+        candies.push_back({x, cnt});
+    }
+    sort(begin(candies), end(candies)); // sort by position
+    
+    auto distance = [&](int idx, int jdx){
+        int x1 = candies[idx].first;
+        int x2 = candies[jdx].first;
+        return x2 - x1;
+    };
+
+    int answer = 0;
+    int cnt = 0;
+    int j = 0;
+    
+    for(int i=0; i<n; ++i){
+        while(j < n && distance(i, j) <= 2*k){
+            cnt += candies[j].second;
+            ++j;
+            answer = max(answer, cnt);
+        }
+        cnt -= candies[i].second;
+    }
+    cout << answer << '\n';
+    return 0;
+}
